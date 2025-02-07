fab923040c10f749995a6f0f799c93bfd5021e5f
Vibol1 <86060974+Vibol1@users.noreply.github.com>
2025-02-06 19:57:37 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "ចុចyesទៅនៅចាំអីទៀត",
+    "ចុចyesមេីល",
+    "អើយចុចyesទៅ",
+    "ចុចyesទៅនៅចាំអីទៀត",
+    "ចុចyesមេីល",
+    "បើអ្នកនិយាយថាទេ ខ្ញុំពិតជាសោកស្ដាយណាស់...",
+    "អើយចុចyesទៅ",
+    "ចុចyesទៅនៅចាំអីទៀត",
+    "ចុចyesមេីល",
+    "ចុចyesទៅនៅចាំអីទៀត❤️"
 ];
 
 let messageIndex = 0;
